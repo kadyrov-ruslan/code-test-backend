9901a0f5e55a7e60697ee04ae62748b0a78b857d
ruslankadyrov <if,fkjdfKadyrov96>
2020-09-13 19:51:01 +0300

    Add IProductApplicationVisitor for different products applications submittion

---
+++ SlothEnterprise.ProductApplication/Services/IProductApplicationVisitor.cs
@@ -0,0 +1,35 @@
+using SlothEnterprise.ProductApplication.Applications;
+using SlothEnterprise.ProductApplication.Products;
+
+namespace SlothEnterprise.ProductApplication.Services
+{
+    /// <summary>
+    /// Provides methods for submit applications of visited products
+    /// </summary>
+    public interface IProductApplicationVisitor
+    {
+        /// <summary>
+        /// Submits application for SelectiveInvoiceDiscount
+        /// </summary>
+        /// <param name="application">Seller application</param>
+        /// <param name="selectiveInvoiceDiscount">Selective invoice discount product</param>
+        /// <returns></returns>
+        int SubmitApplication(ISellerApplication application, SelectiveInvoiceDiscount selectiveInvoiceDiscount);
+        
+        /// <summary>
+        /// Submits application for ConfidentialInvoiceDiscount
+        /// </summary>
+        /// <param name="application">Seller application</param>
+        /// <param name="confidentialInvoiceDiscount">Confidential invoice discount product</param>
+        /// <returns></returns>
+        int SubmitApplication(ISellerApplication application, ConfidentialInvoiceDiscount confidentialInvoiceDiscount);
+        
+        /// <summary>
+        /// Submits application for BusinessLoans
+        /// </summary>
+        /// <param name="application">Seller application</param>
+        /// <param name="businessLoans">Business loans product</param>
+        /// <returns></returns>
+        int SubmitApplication(ISellerApplication application, BusinessLoans businessLoans);
+    }
+}
