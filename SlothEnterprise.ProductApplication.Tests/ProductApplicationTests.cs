9508f9b728dbe50c987ccda95cf5cac67d685e83
ruslankadyrov <if,fkjdfKadyrov96>
2020-09-13 20:30:28 +0300

    Add ProductApplicationService unimplemented tests

--- SlothEnterprise.ProductApplication.Tests/ProductApplicationTests.cs
+++ SlothEnterprise.ProductApplication.Tests/ProductApplicationTests.cs
@@ -4,10 +4,66 @@ namespace SlothEnterprise.ProductApplication.Tests
 {
     public class ProductApplicationTests
     {
+        [Fact]
+        public void SubmitApplication_For_SelectiveInvoiceDiscount_Success()
+        {
+            // TODO implement test case
+            Assert.Equal(1, 1);
+        }
 
         [Fact]
-        public void ThisIsTrue()
+        public void SubmitApplication_For_SelectiveInvoiceDiscount_ErrorIfArgsAreInvalid()
         {
+            // TODO implement test case
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void SubmitApplication_For_SelectiveInvoiceDiscount_ErrorIfAnotherReason()
+        {
+            // TODO implement test case
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void SubmitApplication_For_ConfidentialInvoiceDiscount_Success()
+        {
+            // TODO implement test case
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void SubmitApplication_For_ConfidentialInvoiceDiscount_ErrorIfArgsAreInvalid()
+        {
+            // TODO implement test case
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void SubmitApplication_For_ConfidentialInvoiceDiscount_ErrorIfAnotherReason()
+        {
+            // TODO implement test case
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void SubmitApplication_For_BusinessLoans_Success()
+        {
+            // TODO implement test case
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void SubmitApplication_For_BusinessLoans_ErrorIfArgsAreInvalid()
+        {
+            // TODO implement test case
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void SubmitApplication_For_BusinessLoans_ErrorIfAnotherReason()
+        {
+            // TODO implement test case
             Assert.Equal(1, 1);
         }
     }
